f38f77295856b3b2f9e8532b63e580e30b848815
Marco Tulio <mruizq@unah.hn>
2024-06-20 16:29:09 -0600

    Update: :zap: global styles in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     // Optional configuration for Cloudflare adapter (see docs)
   }),
-  output: 'server', // Set to 'server' for Cloudflare Pages compatibility
+  output: 'server' // Set to 'server' for Cloudflare Pages compatibility
   // ... other Astro configurations
+  ,
+  integrations: [tailwind()]
 });
