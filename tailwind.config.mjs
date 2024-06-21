24882823426f6adf66bcf1f33e6671ae3bc01e33
Marco Tulio <mruizq@unah.hn>
2024-06-20 21:32:37 -0600

    Update: :zap: global styles in layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  'primary-120': '#44BD5D',
+		  'neutral-80': '#3E3E40',
+		},
+	  },
+	},
+	variants: {
+	  extend: {},
 	},
 	plugins: [],
-}
+  }
