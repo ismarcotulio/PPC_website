0bfdcbe4f791da8bc2ca83ec7e77b1758ccef946
Marco Tulio <mruizq@unah.hn>
2024-06-21 01:05:04 -0600

    Feature: :sparkles: Slider content added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,9 +6,28 @@ export default {
 	theme: {
 	  extend: {
 		colors: {
+		  'primary-190': '#132E18',
 		  'primary-120': '#44BD5D',
 		  'neutral-80': '#3E3E40',
 		},
+		height: {
+			'500px': '500px',
+			'300px': '300px',
+			'200px': '200px',
+			'100px': '100px',
+			'70px': '70px',
+			'60px': '60px',
+			'40px': '40px',
+		  },
+		width: {
+			'100px': '100px'
+		},
+		space: {
+			'70px': '70px'
+		},
+		grayscale: {
+			50: '50%',
+		  }
 	  },
 	},
 	variants: {
